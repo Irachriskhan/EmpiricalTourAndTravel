4c2555ef890c9eed3f508d111efbb0ce89cd957e
Christophe Irakoze <irachriskhan@gmail.com>
2024-01-10 14:34:57 +0200

    add components

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import "./header.css";
+
+const nav_links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <container>
+        <Row>
+          <div className="nav_wrapper d-flex align-items-center justify-content-between">
+            {/* --------------- logo ---------------- */}
+            <div className="logo">
+              <img src={logo} alt="" style={{ height: "60px" }} />
+            </div>
+            {/* --------------- logo end---------------- */}
+            {/* --------------- menu start---------------- */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav_links.map((item, index) => {
+                  return (
+                    <li className="nav__item" key={index}>
+                      <NavLink
+                        to={item.path}
+                        className={(navClass) =>
+                          navClass.isActive ? "active__link" : ""
+                        }
+                      >
+                        {item.display}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            {/* --------------- menu end---------------- */}
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </container>
+    </header>
+  );
 };
 
 export default Header;
