0e35c05827c9f2c8f278d8a4aa1c748846048e4f
Christophe Irakoze <irachriskhan@gmail.com>
2024-01-12 16:06:49 +0200

    finalize the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "../styles/home.css";
 import { Container, Row, Col } from "reactstrap";
+
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle";
+import experienceImg from "../assets/images/experience.png";
 
 import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonial/Testimonials";
+import Newsletter from "../shared/Newsletter";
 
 const Home = () => {
   return (
@@ -54,14 +61,107 @@ const Home = () => {
         </Container>
       </section>
       {/* ================= hero section end================ */}
-      <Container>
-        <Row>
-          <Col lg="3">
-            <h5 className="services__subtitle">What we serve</h5>
-            <h2 className="services__title">What we offer our best services</h2>
-          </Col>
-        </Row>
-      </Container>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">
+                What we offer our best services
+              </h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+      {/* ================= featured tour section start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+      {/* ================= featured tour section end================ */}
+
+      {/* ================= experience section start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With our experience <br /> We will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> Suscipit blanditiis, repellat facilis commodi eveniet
+                  quia libero pariatur!
+                </p>
+              </div>
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>100</span>
+                  <h6>Successful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>10</span>
+                  <h6>Regular clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>8</span>
+                  <h6>Years of experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* ================= experience section end================ */}
+      {/* ================= gallery section start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesGallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* ================= gallery section end================ */}
+
+      {/* ================= testimonial section start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Fans Love"} />
+              <h2 className="testimonial__title">What our fans say about us</h2>
+            </Col>
+            <Col lg="12">
+              <Testimonials />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* ================= testimonial section end================ */}
+      <Newsletter />
     </>
   );
 };
--- frontend/src/services/ServiceCard.jsx
+++ frontend/src/services/ServiceCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./service-card.css";
 
-const ServiceCard = () => {
-  return <div>ServiceCard</div>;
+const ServiceCard = ({ item }) => {
+  const { imgUrl, title, desc } = item;
+  return (
+    <div className="service__item">
+      <div className="service__img">
+        <img src={imgUrl} alt="" />
+      </div>
+      <h5>{title}</h5>
+      <p>{desc}</p>
+    </div>
+  );
 };
 
 export default ServiceCard;
