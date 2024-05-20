91e07b5d3d26cf47410ae7291c1b51d5cafbb2fe
Christophe Irakoze <irachriskhan@gmail.com>
2024-05-20 12:50:14 +0200

    update the presets

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,12 +2,15 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-    [
-      "@babel/preset-react",
-      {
-        deployment: process.env.BABEL_ENV === "deployment",
-      },
-    ],
+    // [
+    //   "@babel/preset-react",
+    //   {
+    //     production: process.env.BABEL_ENV === "production",
+    //   },
+    // ],
+    {
+      presets: ["@babel/preset-react", "@babel/preset-env"],
+    },
   ];
 
   const plugins = [
