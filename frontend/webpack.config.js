8db5cacc6a8cbb4e087525ebbee302cbf7d96abd
Christophe Irakoze <irachriskhan@gmail.com>
2024-05-20 13:32:40 +0200

    reset configuration file

---
+++ frontend/webpack.config.js
@@ -0,0 +1,26 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("babel-loader"),
+            options: {
+              plugins: [
+                isDevelopment && require.resolve("react-refresh/babel"),
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [isDevelopment && new ReactRefreshWebpackPlugin()].filter(Boolean),
+};
