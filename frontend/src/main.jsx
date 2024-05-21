036d22fb531cd6ddbdfd5839e6c5a7ffad801492
Christophe Irakoze <irachriskhan@gmail.com>
2024-05-21 03:39:33 +0200

    change the port number

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,4 +16,16 @@ export default defineConfig({
       ".scss",
     ],
   },
+  server: {
+    port: 3000,
+  },
 });
+
+// open: true,
+// proxy: {
+//   "/api": {
+//     target: "http://localhost:3001",
+//     changeOrigin: true,
+//     rewrite: (path) => path.replace(/^\/api/, ""),
+//   },
+// },
