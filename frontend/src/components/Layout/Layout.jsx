9f1b2e71f6301ec12ea31725abc4b6e6a5885b47
Iriboneye-Nina <ninairiboneye@gmail.com>
2024-08-07 16:45:40 +0200

    layout-and-login-upadated

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,21 +1,26 @@
-// import React from "react";
+
 import Header from "../Header/Header";
 import Routers from "../../router/Routers.jsx";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
-  // CHECH the ROLE if no token or user == user => route of users
-  // if user == admin 
-  let data = localStorage.getItem("user");
-  console.log("data",data)
-  const result = JSON.stringify(data);
-  if ( data.role === "user") {
-    return (
-      <>
-        <Routers />
-      </>
-    );
-  } else {
+ 
+  // let data = localStorage.getItem("user");
+  // let user = null;
+
+  // if (data) {
+  //   user = JSON.parse(data); // Parse the data correctly
+  // }
+
+  // // If user is not available or role is 'user', return the user's layout
+  // if (user && user.role === "user") {
+  //   return (
+  //     <>
+  //       <Routers />
+  //     </>
+  //   );
+  // } else {
+  //   
     return (
       <>
         <Header />
@@ -24,7 +29,5 @@ const Layout = () => {
       </>
     );
   }
-  
-};
-export default Layout;
 
+export default Layout;
